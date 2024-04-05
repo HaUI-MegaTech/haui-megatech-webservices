35da3be74283799e506b647ceef728ea0721350e
nvhoang295 <hoangnv.swe@gmail.com>
2024-04-05 23:32:55 +0700

    init base project

---
+++ src/main/java/shop/haui_megatech/HauiMegatechWebservicesApplication.java
@@ -0,0 +1,13 @@
+package shop.haui_megatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HauiMegatechWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HauiMegatechWebservicesApplication.class, args);
+	}
+
+}

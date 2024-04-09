bdb4cf1540476f0d68f0bd6c645f0636c195c6f1
Nguyen Viet Hoang <hoangnv.swe@gmail.com>
2024-04-09 14:56:50 +0700

    modify cors filter in order to fetch from localhost client

--- src/main/java/shop/haui_megatech/security/ApplicationSecurityConfig.java
+++ src/main/java/shop/haui_megatech/security/ApplicationSecurityConfig.java
@@ -2,9 +2,12 @@ package shop.haui_megatech.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class ApplicationSecurityConfig {
@@ -12,11 +15,13 @@ public class ApplicationSecurityConfig {
 	@Bean
 	SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		return http
-				.csrf(csrf -> csrf.disable())
-				.cors(cors -> cors.disable())
-				.sessionManagement(management -> 
+				.csrf(AbstractHttpConfigurer::disable)
+				.sessionManagement(management ->
 					management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(authz -> authz.anyRequest().permitAll())
 				.build();
 	}
+
+
+
 }

e2cf2fd3776e09b04d1acb9fde5970af2a32c3d0
Nguyen Viet Hoang <hoangnv.swe@gmail.com>
2024-04-10 00:36:34 +0700

    reformat code

--- src/main/java/shop/haui_megatech/constant/ErrorMessageConstant.java
+++ src/main/java/shop/haui_megatech/constant/ErrorMessageConstant.java
@@ -1,9 +1,9 @@
 package shop.haui_megatech.constant;
 
 public class ErrorMessageConstant {
-	
-	
-	public static class Product {
-		public static final String NOT_FOUND = "product.error.not_found";
-	}
+
+
+    public static class Product {
+        public static final String NOT_FOUND = "product.error.not_found";
+    }
 }
--- src/main/java/shop/haui_megatech/constant/UrlConstant.java
+++ src/main/java/shop/haui_megatech/constant/UrlConstant.java
@@ -1,15 +1,15 @@
 package shop.haui_megatech.constant;
 
-public class UrlConstant {	
-	public static class Product {
-		private static final String PREFIX = "/products";
-		
-		public static final String GET_PRODUCTS = PREFIX;
-		public static final String GET_PRODUCT = PREFIX + "/{productId}";
-		public static final String CREATE_PRODUCT = PREFIX;
-		public static final String UPDATE_PRODUCT = PREFIX + "/{productId}";
-		public static final String DELETE_PRODUCT = PREFIX + "/{productId}";
-		
-	}
+public class UrlConstant {
+    public static class Product {
+        private static final String PREFIX = "/products";
+
+        public static final String GET_PRODUCTS   = PREFIX;
+        public static final String GET_PRODUCT    = PREFIX + "/{productId}";
+        public static final String CREATE_PRODUCT = PREFIX;
+        public static final String UPDATE_PRODUCT = PREFIX + "/{productId}";
+        public static final String DELETE_PRODUCT = PREFIX + "/{productId}";
+
+    }
 
 }

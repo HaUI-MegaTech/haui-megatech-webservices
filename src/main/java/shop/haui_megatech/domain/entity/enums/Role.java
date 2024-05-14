cb984c2adee7aed1573ad82c3666233de030c53a
Nguyen Viet Hoang <hoangnv.swe@gmail.com>
2024-05-14 16:35:17 +0700

    Sketch access permission matrix

--- src/main/java/shop/haui_megatech/domain/entity/enums/Role.java
+++ src/main/java/shop/haui_megatech/domain/entity/enums/Role.java
@@ -6,11 +6,6 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum Role {
-    ROLE_CUSTOMER(1),
-    ROLE_STAFF(2),
-    ROLE_MANAGER(3),
-    ROLE_ADMIN(4),
-    ROLE_PRESIDENT(5);
-
-    private final Integer priority;
+    ROLE_CUSTOMER,
+    ROLE_ADMIN,
 }
